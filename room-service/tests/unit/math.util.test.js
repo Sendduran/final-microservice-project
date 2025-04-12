60962604303f8b033f1da345362a8b6420ddf623
Sendduran Thailan <dsendduran@gmail.com>
2025-04-12 02:29:05 -0400

    Trigger CI/CD test

--- room-service/tests/unit/math.util.test.js
+++ room-service/tests/unit/math.util.test.js
@@ -1,7 +1,7 @@
 const { add } = require("../../src/utils/math");
 
 describe("add()", () => {
-  it("adds two numbers correctly", () => {
+  it("addstwo numbers correctly", () => {
     expect(add(2, 3)).toBe(5);
   });
 });
